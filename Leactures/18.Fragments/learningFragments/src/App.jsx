0b5b77f101fd444f49cbee09778eedddf82be652
vedantgade555 <vedantgade5555@gmail.com>
2025-08-19 20:06:33 +0530

    End the Fragments Concept
    There are two Fragmnts methods  first is long and another is dhort I'll describe both

--- Leactures/18.Fragments/learningFragments/src/App.jsx
+++ Leactures/18.Fragments/learningFragments/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -6,7 +7,48 @@ function App() {
   
 
   return (
-    <h1>Fragments</h1>
+  //  <div>
+  //    <h1>Healthy Foods</h1>
+  //     <ul class="list-group">
+  //       <li class="list-group-item">An item</li>
+  //       <li class="list-group-item">A second item</li>
+  //       <li class="list-group-item">A third item</li>
+  //       <li class="list-group-item">A fourth item</li>
+  //       <li class="list-group-item">And a fifth one</li>
+  //     </ul>
+  //   </div>
+
+  // Fragments are used to avoid unneceserily  div tag
+  // If we dont close in div tag then they will show error
+  // Due to that we are use fragments
+
+  // <React.Fragment>
+  //   <h1>Healthy Foods</h1>
+  //      <ul class="list-group">
+  //        <li class="list-group-item">Panner</li>
+  //        <li class="list-group-item">Green Vegitabels</li>
+  //        <li class="list-group-item">Salad</li>
+  //        <li class="list-group-item">Milk</li>
+  //        <li class="list-group-item">Proteins </li>
+  //      </ul>
+  // </React.Fragment>
+  // The above syntax is long syntax
+  // For use of long syntax we need to import the the react file 
+  // The file is import React from "react"
+
+
+  // Another Short Syntax is enclosed in  <></> 
+
+  <>
+    <h1>Healthy Foods</h1>
+       <ul class="list-group">
+         <li class="list-group-item">Panner</li>
+         <li class="list-group-item">Green Vegitabels</li>
+         <li class="list-group-item">Salad</li>
+         <li class="list-group-item">Milk</li>
+         <li class="list-group-item">Proteins </li>
+       </ul>
+  </>
   );
 }
 
