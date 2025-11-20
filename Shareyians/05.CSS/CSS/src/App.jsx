2a4cf7feb30c56ae5edaa3778f677e7419059133
vedantgade555 <vedantgade5555@gmail.com>
2025-11-20 23:14:42 +0530

    .

---
+++ Shareyians/05.CSS/CSS/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Button from './components/button/Button'
+
+function App() {
+  return (
+    <>
+      <Button></Button>
+    </>
+  )
+}
+
+export default App
---
+++ Shareyians/05.CSS/CSS/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {}
+
+const Button = () => {
+  return (
+    <div className='btn'>
+        This is Button
+    </div>
+  )
+}
+
+export default Button
