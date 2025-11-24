b48923cf15bb9044d8d688aac54bc7287c06ba69
vedantgade555 <vedantgade5555@gmail.com>
2025-11-24 23:38:50 +0530

    making an Left card section of an project

---
+++ Shareyians/07.UiProject/UI-Project/src/components/Section1/RightCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightCard = () => {
+  return (
+    <div className='h-full w-80 bg-red-500 rounded-4xl'></div>
+  )
+}
+
+export default RightCard
---
+++ Shareyians/07.UiProject/UI-Project/src/components/Section1/RightContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightContent = () => {
+  return (
+    <div className='h-full w-2/3 bg-blue-200 p-6'>
+        <RightContent></RightContent>
+    </div>
+  )
+}
+
+export default RightContent
